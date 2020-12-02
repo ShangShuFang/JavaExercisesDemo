82e593cc27ac94deb910faf287c454085aeba455
zhangqiang <johnny.q.zhang@outlook.com>
2020-12-02 13:03:58 +0800

    java随堂习题（编程题）参考答案

--- com/shangshufang/homework/step1/knowledge9001_01/Demo1.java
+++ com/shangshufang/homework/step1/knowledge9001_01/Demo1.java
@@ -5,12 +5,6 @@ package com.shangshufang.homework.step1.knowledge9001_01;
  */
 public class Demo1 {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            for (String arg : args) {
-                System.out.println("arg = " + arg);
-            }
-        }
-//        System.out.println("不放弃逛街、睡觉、打游戏，怎么遇见未来更好的自己！");
-        System.out.println("args.l="+args.length);
+        System.out.println("不放弃逛街、睡觉、打游戏，怎么遇见未来更好的自己！");
     }
 }
